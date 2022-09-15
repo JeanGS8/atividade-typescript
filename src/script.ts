47dfb93f48b6cb21669b197d0f9b046582de4dfa
Jean <jean118877@hotmail.com>
2022-09-15 15:48:44 -0300

    Exercício concluído

---
+++ src/script.ts
@@ -0,0 +1,10 @@
+let idade: Number = 27;
+const nome: string = 'Jean';
+const maiorDeIdade: boolean = true;
+
+function soma (n1: number, n2: number): number{
+    const soma = n1 + n2;
+    return soma;
+}
+    
+document.write(`A soma de 7 + 5 é = ${soma(7, 5)}`);
